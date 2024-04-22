16a88fe1adeae045f01a0952e501af0d1aa8ce2b
difiore <anthony.difiore@gmail.com>
2024-04-22 13:57:22 -0500

    commit

--- src/lm.R
+++ src/lm.R
@@ -1,23 +1,23 @@
 library(shiny)
 library(DT)
 library(tidyverse)
-f <- "https://raw.githubusercontent.com/difiore/ADA-datasets/master/zombies.csv"
-z <- read_csv(f, col_names = TRUE)
-z <- select(z, height, weight, age, gender, major)
-z$gender <- factor(z$gender)
-z$major <- factor(z$major)
+library(broom)
+f <- "https://raw.githubusercontent.com/difiore/ada-2024-datasets/main/zombies.csv"
+d <- read_csv(f, col_names = TRUE)
+d <- select(d, height, weight, age, gender, major)
+d$gender <- factor(d$gender)
+d$major <- factor(d$major)
 r <- c("height", "weight", "age")
-p <- names(z)
+p <- names(d)
 # Define the UI ----
 ui <- fluidPage(
   titlePanel(h1("Simple LM Visualizer")),
   sidebarLayout(
-    sidebarPanel(
+    sidebarPanel(width = 5,
       selectInput(
         "response",
         label = "Choose a response variable...",
         choices = c("", r)
-        # NOTE: the "" is needed to allow NO VARIABLE to be the default value
       ),
       br(),
       selectInput(
@@ -32,7 +32,7 @@ ui <- fluidPage(
       tableOutput("modelresults"),
       style="text-align:center"
     ),
-    mainPanel(
+    mainPanel(width = 7,
       dataTableOutput("datatable"),
       plotOutput("plot")
     )
@@ -43,7 +43,7 @@ ui <- fluidPage(
 server <- function(input, output) {
 
   output$datatable <-
-    renderDataTable(z, options = list(
+    renderDataTable(d, options = list(
       paging = TRUE,
       lengthMenu = list(c(5, 10, 25, -1), c('5', '10', '25', 'All')),
       pageLength = 5
@@ -68,10 +68,8 @@ server <- function(input, output) {
   
   output$modelresults <- renderTable({
     if (!is.null(m())) {
-      res <- lm(data = z, formula = m())
-      res <- as.data.frame(coefficients(res))
-      names(res) <- "Beta"
-      res
+      res <- lm(data = d, formula = m())
+      tidy(res) |> select(term, estimate, p.value)
     }
   }, width = "100%", rownames = TRUE, striped = TRUE, spacing = "s", bordered =
     TRUE, align = "c", digits = 3)
@@ -80,13 +78,14 @@ server <- function(input, output) {
     if (!is.null(m()) & length(input$predictors) == 1) {
       y <- input$response
       x <- input$predictors
-      if (class(z[[x]]) != "factor") {
-        p <- ggplot(data = z, aes(x = z[[x]], y = z[[y]])) +
+      if (class(d[[x]]) != "factor") {
+        p <- ggplot(data = d, aes(x = .data[[x]], y = .data[[y]])) +
           geom_point() +
           geom_smooth(method = lm)
       } else {
-        p <- ggplot(data = z, aes(x = z[[x]], y = z[[y]])) +
-          geom_boxplot()
+        p <- ggplot(data = d, aes(x = .data[[x]], y = .data[[y]])) +
+          geom_violin() +
+          geom_jitter(width = 0.2, alpha = 0.5)
       }
       p <- p + xlab(x) + ylab(y) +
         theme(axis.text.x = element_text(angle = 90, hjust = 1)) 
@@ -94,22 +93,23 @@ server <- function(input, output) {
     } else if (!is.null(m()) & length(input$predictors) == 2) {
       y <- input$response
       x <- input$predictors
-      if (class(z[[x[1]]]) == "factor" & class(z[[x[2]]]) == "factor") {
-        p <- ggplot(data = z, aes(x = z[[x[1]]], y = z[[y]])) +
-          geom_boxplot() +
-          facet_wrap(~ z[[x[2]]])
+      if (class(d[[x[1]]]) == "factor" & class(d[[x[2]]]) == "factor") {
+        p <- ggplot(data = d, aes(x = .data[[x[1]]], y = .data[[y]])) +
+          geom_violin() +
+          geom_jitter(width = 0.2, alpha = 0.5) +
+          facet_wrap(~ d[[x[2]]])
         p <- p + xlab(x[1]) + ylab(y)
-      } else if (class(z[[x[1]]]) != "factor" & class(z[[x[2]]]) == "factor"){
-        p <- ggplot(data = z, aes(x = z[[x[1]]], y = z[[y]])) +
+      } else if (class(d[[x[1]]]) != "factor" & class(d[[x[2]]]) == "factor"){
+        p <- ggplot(data = d, aes(x = .data[[x[1]]], y = .data[[y]])) +
           geom_point() +
           geom_smooth(method = lm) +
-          facet_wrap(~ z[[x[2]]])
+          facet_wrap(~ d[[x[2]]])
         p <- p + xlab(x[1]) + ylab(y)
-      } else if (class(z[[x[1]]]) == "factor" & class(z[[x[2]]]) != "factor"){
-        p <- ggplot(data = z, aes(x = z[[x[2]]], y = z[[y]])) +
+      } else if (class(d[[x[1]]]) == "factor" & class(d[[x[2]]]) != "factor"){
+        p <- ggplot(data = d, aes(x = .data[[x[2]]], y = .data[[y]])) +
           geom_point() +
           geom_smooth(method = lm) +
-          facet_wrap(~ z[[x[1]]])
+          facet_wrap(~ d[[x[1]]])
         p <- p + xlab(x[2]) + ylab(y)
       } else {
         p <- NULL
